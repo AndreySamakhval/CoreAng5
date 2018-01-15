02427acf9b119680bc239d691adc849c898eb5d7
samohvalanAasadadsdf <samohvalan@SAMOKHVAL-AN>
2018-01-15 14:20:28 +0300

    Modifi webpack

--- CoreAng5/CoreAng5/ClientApp/app/app.module.js
+++ CoreAng5/CoreAng5/ClientApp/app/app.module.js
@@ -8,17 +8,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-var AppModule = (function () {
+var AppModule = /** @class */ (function () {
     function AppModule() {
     }
+    AppModule = __decorate([
+        NgModule({
+            imports: [BrowserModule, FormsModule],
+            declarations: [AppComponent],
+            bootstrap: [AppComponent]
+        })
+    ], AppModule);
     return AppModule;
 }());
-AppModule = __decorate([
-    NgModule({
-        imports: [BrowserModule, FormsModule],
-        declarations: [AppComponent],
-        bootstrap: [AppComponent]
-    })
-], AppModule);
 export { AppModule };
 //# sourceMappingURL=app.module.js.map
--- CoreAng5/CoreAng5/webpack.config.js
+++ CoreAng5/CoreAng5/webpack.config.js
@@ -1,6 +1,9 @@
 ﻿var path = require('path');
 var webpack = require('webpack');
 var UglifyJSPlugin = require('uglifyjs-webpack-plugin'); // плагин минимизации
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
     entry: {
         'polyfills': './ClientApp/polyfills.ts',
@@ -9,7 +12,10 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './wwwroot/dist'),     // путь к каталогу выходных файлов - папка public
         publicPath: '/dist/',
-        filename: "[name].js"       // название создаваемого файла
+        filename: '[name].js'       // название создаваемого файла
+    },
+    devServer: {
+        historyApiFallback: true,
     },
     resolve: {
         extensions: ['.ts', '.js']
@@ -29,6 +35,9 @@ module.exports = {
                 test: /\.html$/,
                 loader: 'html-loader'
             }, {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'file-loader?name=assets/[name].[hash].[ext]'
+            }, {
                 test: /\.css$/,
                 include: path.resolve(__dirname, 'ClientApp/app'),
                 loader: 'raw-loader'
@@ -38,12 +47,22 @@ module.exports = {
     plugins: [
         new webpack.ContextReplacementPlugin(
             /angular(\\|\/)core/,
-            path.resolve(__dirname, 'ClientApp'), // каталог с исходными файлами
+            path.resolve(__dirname, 'wwwroot'), // каталог с исходными файлами
             {} // карта маршрутов
         ),
         new webpack.optimize.CommonsChunkPlugin({
             name: ['app', 'polyfills']
         }),
-        new UglifyJSPlugin()
+        //new HtmlWebpackPlugin({
+        //    template: './wwwroot/index.html'
+        //}),
+        new UglifyJSPlugin(),
+        //new ExtractTextPlugin('[name]..css'),
+        new webpack.NoEmitOnErrorsPlugin()
+        //new webpack.LoaderOptionsPlugin({
+        //    htmlLoader: {
+        //        minimize: false
+        //    }
+        //})
     ]
 }
